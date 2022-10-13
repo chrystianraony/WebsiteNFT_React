660afd7991e385ebd70034ee0887b71d721cc634
Chrystian Raony Perazzoli <chrystian.raony@gmail.com>
2022-10-13 17:52:21 -0300

    Created InfoSection, modified common and assets

--- WebsiteNFT/src/components/InfoSection/index.tsx
+++ WebsiteNFT/src/components/InfoSection/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import INFO_ITEMS from '../../data/infoItems'
+import InfoItem from './infoItem'
+import "./infoSection.css"
 
  export const InfoSection = () => {
   return (
     <div className='info-section'>
-      <div>
+      <div className='is-heading absolute-center'>
         <span className='heading-gradient'>Create and sell your NFTs</span>
+      </div>
+      <div className='is-items-container'>
+        {INFO_ITEMS.map((_infoItem) => (
+        <InfoItem item={_infoItem} />
+        ))}
+         
       </div>
     </div>
   )
--- WebsiteNFT/src/data/infoItems.ts
+++ WebsiteNFT/src/data/infoItems.ts
@@ -1,20 +1,22 @@
-export const INFO_ITEMS = [
+
+const INFO_ITEMS = [
   {
     id:1,
-    icon: require('../assets/wallet.png'),
+    icon: '../assets/icons/wallet.png',
     section_title: "Set up your wallet",
     description: "Once you've set up your wallet of choice, connect it to OpenSea by clicking the wallet icon in the top right corner. Learn about the wallets we support",
   },
   {
     id:2,
-    icon: require('../assets/upload.png'),
+    icon: '../assets/icons/upload.png',
     section_title: "Upload & Create Collection",
     description: "Upload your work then Click My Collections and set up your collection. Add social links, a description, profile & banner images, and set a secondary sales fee",
   },
   {
     id:3,
-    icon: require('../assets/list.png'),
+    icon: '../assets/icons/list.png',
     section_title: "List them for sale",
     description: "Choose between auctions, fixed-price listings, and declining-price listings. You choose how you want to sell your NFTs, and we help you sell them",
   },
 ]
+export default INFO_ITEMS
